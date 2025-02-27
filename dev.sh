1b924024cd7d8a046e599d7ad462eaadc45382ec
wuzhiping <wuzhiping@users.noreply.github.com>
2025-02-27 15:51:51 +0800

    py312

--- dev.sh
+++ dev.sh
@@ -3,5 +3,5 @@
 docker run --rm -it -p 5000:8888 -p 8000:8000 \
                                  -v $PWD:/src \
                               shawoo/agent \
-                              conda run -n py310 -v --live-stream \
+                              conda run -n py312 -v --live-stream \
                               jupyter lab --allow-root --ip=0.0.0.0 --NotebookApp.token='12345678'
